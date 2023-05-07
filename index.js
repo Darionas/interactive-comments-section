df3559739b1c2dfa72440b1a28cca9ef794d4271
Dario <darionas@gmail.com>
2023-05-07 21:10:03 +0300

    update index.html_4, README.md_3, add index.css, animo.css, index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+'Use strict'
+
+const juliusomo = document.getElementsByClassName('juliusomo')[0];
+const amyrobson = document.getElementsByClassName('amyrobson')[0];
+const maxblagun = document.getElementsByClassName('maxblagun')[0];
+const ramsesmiron = document.getElementsByClassName('ramsesmiron')[0];
+
+juliusomo.addEventListener('animationstart', function(){
+    juliusomo.style.visibility = 'visible';
+}, false)
+
+amyrobson.addEventListener('animationstart', function(){
+    amyrobson.style.visibility = 'visible';
+}, false)
+
+maxblagun.addEventListener('animationstart', function(){
+    maxblagun.style.visibility = 'visible';
+}, false)
+
+ramsesmiron.addEventListener('animationstart', function(){
+    ramsesmiron.style.visibility = 'visible';
+}, false)
