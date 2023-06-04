93498500c557ff3c7e287401493e443f23c86916
Dario <darionas@gmail.com>
2023-06-04 23:16:48 +0300

    update data.json_2, index.css_14, index.js_10

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ members();
 
 
 
-let myData, comments, comm,ownerImage, comment_reply, reply, currentUser, comment_container, comment_container_reply, reply_container,
-comment_reply_owner, reply_reply, reply_owner;
+let get, get_update, myData, comments, comm, ownerImage, comment_reply, reply, currentUser, comment_container, comment_container_reply, reply_container,
+comment_reply_owner, reply_reply, reply_owner, member, comment_header_owner, currentUser_update;
 let x = 0;
 
 
@@ -24,14 +24,15 @@ fetch('data.json')
         }
 
         myData = localStorage.getItem('data');
+        
 
         if(localStorage.getItem('data')) {
-            const get = JSON.parse(myData);
-            console.log(get);
+            get = JSON.parse(myData);
+            //console.log(get);
             comments = get.comments;
             currentUser = get.currentUser;       
             exec();
-        
+            go();
         }
     })
 
@@ -40,13 +41,11 @@ fetch('data.json')
 function exec() {
     comments.forEach((item) => {
         let n = 0;
-        //console.log(item);
-
         //Comment without reply construct
         if(item.replies.length === 0) {
-            //console.log(item.replies);
+            console.log(users[x].username);
             comment_container =
-                `<div class='comment' data-id='${item.id}' style='margin: 0 0 1rem 0;'>
+                `<div class='comment' id='${item.id}' style='margin: 0 0 1rem 0;'>
                     <div class="comment_header">
                         <picture class="comment_header-picture">
                             <source srcset="${item.user.image.webp}" type="image/webp">
@@ -55,7 +54,7 @@ function exec() {
                         </picture>
                         <div class="comment_header-container">
                             <span class="comment_header-name">${item.user.username}</span>
-                            <span class="comment_header-owner">you</span>
+                            ${item.user.username === users[x].username ? `<span class="comment_header-owner">you</span>` : `<span class="comment_header-owner" style="display: none">you</span>`}
                             <span class="comment_header-time">${item.createdAt}</span>
                         </div>
                     </div>
@@ -81,7 +80,7 @@ function exec() {
         if(item.replies.length > 0) {
             //console.log(item.replies);  
             comment_container_reply = 
-                `<div class='comment' data-id='${item.id}' style='margin: 0 0 1rem 0;'>
+                `<div class='comment' id='${item.id}' style='margin: 0 0 1rem 0;'>
                     <div class="comment_header">
                         <picture class="comment_header-picture">
                             <source srcset="${item.user.image.webp}" type="image/webp">
@@ -90,7 +89,7 @@ function exec() {
                         </picture>
                         <div class="comment_header-container">
                             <span class="comment_header-name">${item.user.username}</span>
-                            <span class="comment_header-owner">you</span>
+                            ${item.user.username === users[x].username ? `<span class="comment_header-owner">you</span>` : `<span class="comment_header-owner" style="display: none">you</span>`}
                             <span class="comment_header-time">${item.createdAt}</span>
                         </div>
                     </div>
@@ -116,27 +115,27 @@ function exec() {
             n++;
             reply = comments[n].replies;
             //console.log(reply);
-            reply.forEach((items) => {
+            reply.forEach((item) => {
                 reply_container =
                     `<div class='replies'>
-                        <div class="reply" style='margin: 0 0 1rem 0;'>
+                        <div class="reply" id="${item.id}" style='margin: 0 0 1rem 0;'>
                             <div class="reply_header">
                                 <picture class="reply_header-picture">
-                                    <source srcset="${items.user.image.webp}" type="image/webp">
-                                    <source srcset="${items.user.image.png}" type="image/jpeg"> 
-                                    <img src="${items.user.image.png}" alt="${items.user.username}">
+                                    <source srcset="${item.user.image.webp}" type="image/webp">
+                                    <source srcset="${item.user.image.png}" type="image/jpeg"> 
+                                    <img src="${item.user.image.png}" alt="${item.user.username}">
                                 </picture>
                                 <div class="reply_header-container">
-                                    <span class="reply_header-name">${items.user.username}</span>
+                                    <span class="reply_header-name">${item.user.username}</span>
                                     <span class="reply_header-owner">you</span>
-                                    <span class="reply_header-time">${items.createdAt}</span>
+                                    <span class="reply_header-time">${item.createdAt}</span>
                                 </div>
                             </div>
-                            <div class="reply_content" contenteditable="false"><span class="replayinTo">${'@'+items.replyingTo}</span> ${items.content}</div>
+                            <div class="reply_content" contenteditable="false"><span class="replayinTo">${'@'+item.replyingTo}</span> ${item.content}</div>
                             <button class="reply_update">update</button>
                             <div class="reply_vote">
                                 <span class="reply_vote-plus"><svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path class='plus' d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg></span>
-                                <span class="reply_vote-content">${items.score}</span>
+                                <span class="reply_vote-content">${item.score}</span>
                                 <span class="reply_vote-minus"><svg width="11" height="7" xmlns="http://www.w3.org/2000/svg"><path class="minus" d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg></span>
                             </div>
                             <div class="reply_editor">
@@ -151,6 +150,7 @@ function exec() {
             })
         }
     });
+
 
     //Reply construct
     comment_reply_owner = document.getElementsByClassName('comment');
@@ -197,34 +197,42 @@ function exec() {
 
             x = i.id;
             //console.log(x);
-            go(x);
+           //go(x);
             this.classList.add('userstyle');
 
+            //console.log(users[x].username);
+               get.currentUser.id = users[x].id;              
+               get.currentUser.username = users[x].username;
+               get.currentUser.image.png = users[x].image.png;
+               get.currentUser.image.webp = users[x].image.webp;
+               localStorage.setItem('data', JSON.stringify(get));
+               console.log(currentUser.id);
+               go();
         });  
     });
     
-    //populate one instance of object in javaScript
-    function go(y) {
-        for(let e=0; e < users.length; e++) {
-            users[e];
-        }
-
-        //console.log(users[y].image.png);
-        //console.log(y);
-                
-            ownerImage = 
-                `<picture class="main_respond-picture">
-                    <source srcset="${users[y].image.webp}" type="image/webp">
-                    <source srcset="${users[y].image.png}" type="image/jpeg"> 
-                    <!--stop animation in inline style-->
-                    <img class="${users[y].username}" src="${users[y].image.png}" alt="${users[y].username}" style="visibility: visible; animation-duration: 0s; !important;">
-                </picture>
-                <textarea class="main_respond-content" rows="3" aria-label="Write comment" placeholder="Add a comment..."></textarea>
-                <button class="send">send</button>`.trim();
-            
-                document.querySelector('.main_respond').innerHTML = ownerImage;
-    }
-
-    go(x);
 }
+
+
+
+function go() {
+    get_update = JSON.parse(localStorage.getItem('data'));
+    //console.log(get_update);
+    currentUser_update = get_update.currentUser;
+    
+    ownerImage = 
+    `<picture id="${currentUser_update.id}" class="main_respond-picture">
+        <source srcset="${currentUser_update.image.webp}" type="image/webp">
+        <source srcset="${currentUser_update.image.png}" type="image/jpeg"> 
+        <!--stop animation in inline style-->
+        <img class="${currentUser_update.username}" src="${currentUser_update.image.png}" alt="${currentUser_update.username}" style="visibility: visible; animation-duration: 0s; !important;">
+    </picture>
+    <textarea class="main_respond-content" rows="3" aria-label="Write comment" placeholder="Add a comment..."></textarea>
+    <button class="send">send</button>`.trim();
+
+    document.querySelector('.main_respond').innerHTML = ownerImage;
+
+   
+}
+
 
