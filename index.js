bc0deba2b05bc0872bf74f015e0bdb7772f2a900
Dario <darionas@gmail.com>
2023-11-05 11:38:34 +0200

    modified_43: index.js

--- index.js
+++ index.js
@@ -442,7 +442,7 @@ cancel.addEventListener('click', () => {
         //sessionStorage.setItem('wrapId', JSON.stringify(delWrap.id));
         sessionStorage.setItem('userId', JSON.stringify(tor));
         setTimeout(function() {
-            location.reload(true);
+            location.replace(location.href);
         }, 500);
         alert('cancel');
 })
@@ -453,7 +453,7 @@ delet.addEventListener('click', () => {
     modal.classList.remove('show');
     sessionStorage.setItem('flag', JSON.stringify(true));
     setTimeout(function() {
-        location.reload(true);
+        location.replace(location.href);
     }, 500);
 })
 
